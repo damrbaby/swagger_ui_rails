f556b2210db6f188568e941ccb07389ca896c058
Matthew Windwer <mattwindwer@gmail.com>
2013-01-23 20:39:20 -0500

    Default to current url for discovery url

--- app/controllers/swagger_ui_rails/swagger_ui_controller.rb
+++ app/controllers/swagger_ui_rails/swagger_ui_controller.rb
@@ -2,5 +2,9 @@ require_dependency "swagger_ui_rails/application_controller"
 
 module SwaggerUiRails
   class SwaggerUiController < ApplicationController
+    def index
+      @discovery_url = SwaggerUiRails.discovery_url || URI.join(request.url, 'swagger_doc.json')
+      @api_key = SwaggerUiRails.api_key
+    end
   end
 end
